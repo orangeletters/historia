8df828fba51863721cc993cbbeaa3abb67d82eee
orangeletters <orangeletters@proton.me>
2025-01-01 18:23:53 -0500

    first part

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      'midnight-blue': '#07132D',
+      'light-aqua': '#c6fcf7',
+      'vibrant-red': '#f44336',
+    },
+  },
+  plugins: [],
+}
+
