1f3154baea700e7f30ba340c01d2f442fcccf352
orangeletters <orangeletters@proton.me>
2025-01-01 22:20:10 -0500

    first part

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,28 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in': {
+          '0%': {opacity: '0'},
+          '100%': {opacity: '100'},
+        },
+        'fade-color': {
+          '0%': {opacity: '0'},
+          '100%': {opacity: '100', color:'#f44336',},
+        }
+      },
+      animation: {
+        'fadein': 'fade-in 2s',
+        'fade-color': 'fade-color 10s forwards',
+      },
+    },
     colors: {
       'midnight-blue': '#07132D',
       'light-aqua': '#c6fcf7',
       'vibrant-red': '#f44336',
     },
+    
   },
   plugins: [],
 }
