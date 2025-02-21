83ba84357295e983cf5e4676ac6158b8133a1b67
orangeletters <orangeletters@proton.me>
2025-02-21 17:05:35 -0500

    avance animacion imagenes de las palabras n y o

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html"],
+  content: ["./**/*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       keyframes: {
@@ -11,11 +11,24 @@ module.exports = {
         'fade-color': {
           '0%': {opacity: '0'},
           '100%': {opacity: '100', color:'#f44336',},
-        }
+        },
+        'levitation-one':{
+          '0%': {transform: 'translateY(10px)'},
+          '50%': {transform: 'translateY(-10px)'},
+          '100%': {transform: 'translateY(10px)'},
+        },
+        'levitation-two':{
+          '0%': {transform: 'translate(-3rem, -10px)'},
+          '50%': {transform: 'translate(-3rem, 10px)'},
+          '100%': {transform: 'translate(-3rem, -10px)'}
+        },
       },
+
       animation: {
         'fadein': 'fade-in 2s',
         'fade-color': 'fade-color 10s forwards',
+        'levitation-one': 'levitation-one 5s  infinite',
+        'levitation-two': 'levitation-two 5s infinite',
       },
 
       colors: {
@@ -25,7 +38,6 @@ module.exports = {
       }
     },
 
-    
   },
   plugins: [],
 };
